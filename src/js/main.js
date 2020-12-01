52709adda0f85f9f5fb998b4ae3cc238bfe6878d
mahya <m.kabiri@shab.ir>
2020-12-01 20:13:43 +0330

    remove comment

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,6 @@ let number = 0;
 let container = document.getElementById("container");
 let newCircle;
 
-// function myFunction(event) {
-//     number++;
-//     var x = event.pageX;     // Get the horizontal coordinate
-//     var y = event.pageY;     // Get the vertical coordinate
-//     newCircle = document.createElement("div");
-//     newCircle.setAttribute("id", `node${number}`);
-//     var node = document.createTextNode(number);
-//     newCircle.appendChild(node);
-//     let newStyle = (number % 5 === 0) ? "orange-circle" : "circle";
-//     newCircle.classList.add(newStyle);
-//     newCircle.style.cssText = `top:${y}px;left:${x}px;`
-//     container.appendChild(newCircle);
-// }
 container.addEventListener("click", function(event) {
     number++;
     var x = event.pageX;     // Get the horizontal coordinate
